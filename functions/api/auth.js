2c675847cdb1bece8b818d2850910e4ae9823184
arthurambrosi <arthurambrosi@gmail.com>
2026-02-03 15:32:46 -0300

    Update auth.js

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -4,11 +4,12 @@ export async function onRequest(context) {
 
   try {
     const url = new URL(request.url)
+    const origin = `https://${url.host}`
     const redirectUrl = new URL('https://github.com/login/oauth/authorize')
     const state = crypto.getRandomValues(new Uint8Array(12)).join('')
 
     redirectUrl.searchParams.set('client_id', clientId)
-    redirectUrl.searchParams.set('redirect_uri', `${url.origin}/api/callback`)
+    redirectUrl.searchParams.set('redirect_uri', `${origin}/api/callback`)
     redirectUrl.searchParams.set('scope', 'repo user')
     redirectUrl.searchParams.set('state', state)
 
